555d8b2fafbd235bbceac13239da2476511a1bbd
stephenae8 <stephenlaekins@gmail.com>
2024-10-08 10:31:05 -0400

    add watched feature

--- main.js
+++ main.js
@@ -1,15 +1,35 @@
 $(document).ready(() => {
     getMovies('dragon');
     $('#searchForm').on('submit', (e) => {
+        e.preventDefault();
         const searchText = $('#searchText').val();
         getMovies(searchText);
-        e.preventDefault();
+        $('#movies').show();
+        $('#watchedMovies').hide();
     });
 
     var modals = document.querySelectorAll('.modal')
     modals.forEach(function(modal) {
       new bootstrap.Modal(modal);
     });
+});
+
+$('#watchedButton').click(function(e) {
+    e.preventDefault();
+    if($('#movies').is(':visible')) {
+        $('#movies').hide();
+        $('#watchedMovies').show();
+        displayWatchedMovies();
+    } else{
+        $('#watchedMovies').hide();
+        $('#movies').show();
+    }
+});
+
+$(document).on('click', '.remove-from-watched', function(e) {
+    e.preventDefault();
+    const imdbID = $(this).data('imdbid');
+    removeFromWatched(imdbID);
 });
 
 $(document).on('click', '.movie-details', function(e) {
@@ -19,11 +39,11 @@ $(document).on('click', '.movie-details', function(e) {
 });
 
 $('#searchButton').click(function(e) {
-    console.log('button clicked')
     e.preventDefault();
     const searchText = $('#searchText').val();
     getMovies(searchText);
 });
+
 $(document).on('click', '#loadButton', function(e) {
     console.log('button clicked')
     e.preventDefault();
@@ -80,26 +100,108 @@ function displayMovies() {
     let output = '';
     $.each(allMovies, (index, movie) => {
         output += `
-        <div class="col-md-6 col-md-4 col-lg-3 mb-4">
+        <div class="col-md-6 col-lg-3 mb-4">
             <div class="card h-100 d-flex flex-column text-center">
                 <div class="card-img-top-wrapper">
                     <img src="${movie.Poster === 'N/A' ? 'images/placeholder.png' : movie.Poster}" class="card-img-top" onerror="this.onerror=null; this.src='images/placeholder.png';" alt="${movie.Title} poster">
                 </div>
                 <div class="card-body d-flex flex-column" id="titleBox">
                     <h5 class="card-title flex-grow-1">${movie.Title}</h5>
-                    <a href="#" class="btn btn-primary movie-details" data-imdbid="${movie.imdbID}">More Details</a>
+                    <div class="btn-group" role="group">
+                        <a href="#" class="btn btn-primary movie-details" data-imdbid="${movie.imdbID}">More Details</a>
+                        <button class="btn btn-success add-to-watched" data-imdbid="${movie.imdbID}">Add to Watched</button>
+                    </div>
                 </div>
             </div>
         </div>
         `;
     });
     output += `
-        <div class="col-md-6 col-md-4 col-lg-3 mb-4 loadButton">
+        <div class="col-md-6 col-lg-3 mb-4 loadButton">
             <button type="button" class="btn btn-primary btn-lg" id="loadButton">Load More</button>
         </div>
     `;
 
     $('#movies').html(output);
+    $()
+
+}
+
+$(document).on('click', '.add-to-watched', function(e) {
+    e.preventDefault();
+    const imdbID = $(this).data('imdbid');
+    addToWatched(imdbID);
+});
+
+async function addToWatched(imdbID) {
+    let watchedMovies = JSON.parse(localStorage.getItem('watchedMovies')) || {};
+    
+    if (!watchedMovies[imdbID]) {
+        try {
+            const response = await axios.get(`http://www.omdbapi.com/?apikey=${API_KEY}&i=${imdbID}`);
+            const movieData = response.data;
+            
+            // Store only the necessary data
+            watchedMovies[imdbID] = {
+                Title: movieData.Title,
+                Year: movieData.Year,
+                Poster: movieData.Poster,
+                imdbID: movieData.imdbID,
+                Plot: movieData.Plot,
+                Director: movieData.Director,
+                Actors: movieData.Actors,
+                Genre: movieData.Genre,
+                Rated: movieData.Rated,
+                imdbRating: movieData.imdbRating,
+                Ratings: movieData.Ratings
+            };
+            
+            localStorage.setItem('watchedMovies', JSON.stringify(watchedMovies));
+            alert('Movie added to Watched list!');
+        } catch (error) {
+            console.error('Error fetching movie details:', error);
+            alert('Failed to add movie to Watched list. Please try again.');
+        }
+    } else {
+        alert('This movie is already in your Watched list!');
+    }
+}
+
+function displayWatchedMovies() {
+    let watchedMovies = JSON.parse(localStorage.getItem('watchedMovies')) || {};
+    let output = '';
+
+    for (let imdbID in watchedMovies) {
+        let movie = watchedMovies[imdbID];
+        output += `
+        <div class="col-md-6 col-lg-3 mb-4">
+            <div class="card h-100 d-flex flex-column text-center">
+                <div class="card-img-top-wrapper">
+                    <img src="${movie.Poster === 'N/A' ? 'images/placeholder.png' : movie.Poster}" class="card-img-top" onerror="this.onerror=null; this.src='images/placeholder.png';" alt="${movie.Title} poster">
+                </div>
+                <div class="card-body d-flex flex-column" id="titleBox">
+                    <h5 class="card-title flex-grow-1">${movie.Title}</h5>
+                    <div class="btn-group" role="group">
+                        <a href="#" class="btn btn-primary movie-details" data-imdbid="${movie.imdbID}">More Details</a>
+                        <button class="btn btn-danger remove-from-watched" data-imdbid="${movie.imdbID}">Remove</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+        `;
+    }
+
+    $('#watchedMovies').html(output);
+}
+
+function removeFromWatched(imdbID) {
+    let watchedMovies = JSON.parse(localStorage.getItem('watchedMovies')) || {};
+    if (watchedMovies[imdbID]) {
+        delete watchedMovies[imdbID];
+        localStorage.setItem('watchedMovies', JSON.stringify(watchedMovies));
+        displayWatchedMovies();
+        alert('Movie removed from Watched list!');
+    }
 }
 
 async function movieSelected(imdbID) {
